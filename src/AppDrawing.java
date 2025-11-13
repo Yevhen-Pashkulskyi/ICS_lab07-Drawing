461b5f939ec13366886c3fe097b31de2f02e7b03
Yevhen Pashkulskyi <9349148@stud.op.edu.ua>
2025-11-13 12:38:55 +0200

    save

--- src/AppDrawing.java
+++ src/AppDrawing.java
@@ -6,6 +6,9 @@ import java.util.*;
 
 public class AppDrawing extends JPanel {
 
+    private JTextField x1, y1, x2, y2;
+    private JLabel resultLabel;
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
@@ -16,23 +19,40 @@ public class AppDrawing extends JPanel {
 
     AppDrawing() {
         JFrame frame = new JFrame("Креслення");
-        frame.setSize(400, 400);
+        frame.setSize(800, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
+        JPanel input = new JPanel(new GridLayout(5, 2,10,10));
+        input.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        input.add(new JLabel("x1:"));
+        x1 = new JTextField();
+        input.add(x1);
+        input.add(new JLabel("y1:"));
+        y1 = new JTextField();
+        input.add(y1);
+        input.add(new JLabel("x2:"));
+        x2 = new JTextField();
+        input.add(x2);
+        input.add(new JLabel("y2:"));
+        y2 = new JTextField();
+        input.add(y2);
+
+//        JPanel panelResult = new JPanel();
 
         JButton buttonTask = new JButton("Завдання");
-        JButton button2 = new JButton("Кнопка");
-        JLabel label = new JLabel();
+        JButton buttonCount = new JButton("Розрахунок");
+
+        JTextArea textArea = new JTextArea();
         buttonTask.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                label.setText(printTask());
+                textArea.setText(printTask());
             }
         });
 
-        frame.add(panel);
-        panel.add(buttonTask);
-        panel.add(button2);
-        panel.add(label);
+
+        frame.add(input);
+        input.add(buttonTask);
+        input.add(buttonCount);
+        input.add(textArea);
 
         frame.setVisible(true);
     }
@@ -45,11 +65,10 @@ public class AppDrawing extends JPanel {
             while ((line = reader.readLine()) != null) {
                 sb.append(line).append("\n");
             }
-            return sb.toString();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        return sb.toString();
     }
 
 }
